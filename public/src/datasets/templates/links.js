9e2258d928c9320dd89f175bd223bc1f60550856
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-08-04 10:48:47 +0200

    fix: display only valid links

--- public/src/datasets/templates/links.js
+++ public/src/datasets/templates/links.js
@@ -1,9 +1,18 @@
-export default function ({html, subject}, {landingPage, sparqlEndpoint}) {
-    return html`<span>
-        <a target="_blank" href="${landingPage}">Info</a>
-    </span>
-    <br>
-    <span>
-        SPARQL Endpoint: ${sparqlEndpoint}
-    </span>`
+export default function ({html, subject}, props) {
+    let landingPage = ''
+    if (props.landingPage.value) {
+        landingPage = html`<span>
+            <a target="_blank" href="${props.landingPage.value}">Info</a>
+        </span>
+        <br>`
+    }
+
+    let sparqlEndpoint = ''
+    if (props.sparqlEndpoint.value) {
+        sparqlEndpoint = html`<span>
+            SPARQL Endpoint: <a href="${props.sparqlEndpoint.value}" target="_blank">${props.sparqlEndpoint.value}</a>
+        </span>`
+    }
+
+    return html`${landingPage}${sparqlEndpoint}`
 }
