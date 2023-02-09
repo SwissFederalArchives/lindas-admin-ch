05494ac27b9d64b3062db6c477f97e5516216120
Ludovic Muller <ludovicmuller1@gmail.com>
2023-02-09 11:24:55 +0100

    chore: move some old files in an old directory and remove the rest

--- scripts/build.js
+++
@@ -1,6 +0,0 @@
-const { build } = require('../trifid/esbuild')
-const options = require('../esbuild.config.json')
-
-build(options)
-    .then(() => console.log('Build complete'))
-    .catch(console.error)
