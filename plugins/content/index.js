16b96e2574161af92f00f884e2cd1b15af6386ac
Ludovic Muller <ludovicmuller1@gmail.com>
2023-03-06 10:56:14 +0100

    chore: add h4 and h5 classes

--- plugins/content/index.js
+++ plugins/content/index.js
@@ -27,6 +27,8 @@ const convertToHtml = async (markdownString) => {
       h1: 'h1',
       h2: 'h2',
       h3: 'h3',
+      h4: 'h4',
+      h5: 'h5',
       table: 'table'
     })
     .use(rehypeStringify)
