f0a7fe2eee8d974e42a70b52e8056bd28732517e
Ludovic Muller <ludovicmuller1@gmail.com>
2024-03-06 17:04:32 +0100

    plugins/search: convert to new version

--- plugins/search/index.js
+++ plugins/search/index.js
@@ -4,16 +4,36 @@ import { fileURLToPath } from 'url'
 const currentDir = dirname(fileURLToPath(import.meta.url))
 
 const factory = async (trifid) => {
-  const { render } = trifid
+  const { render, server } = trifid
+  const locals = server.locals
 
-  return async (_req, res, _next) => {
-    res.send(await render(`${currentDir}/view.hbs`, {
-      currentLanguage: res.locals.currentLanguage,
-      defaultLanguage: res.locals.defaultLanguage,
-      locals: res.locals
-    }, {
-      title: 'Datasets'
-    }))
+  return {
+    defaultConfiguration: async () => {
+      return {
+        methods: ['GET'],
+        paths: ['/datasets', '/datasets/']
+      }
+    },
+    routeHandler: async () => {
+      const handler = async (request, reply) => {
+        const fullUrl = `${request.protocol}://${request.hostname}${request.raw.url}`
+        const fullUrlObject = new URL(fullUrl)
+        const fullUrlPathname = fullUrlObject.pathname
+
+        // Enforce trailing slash
+        if (fullUrlPathname.slice(-1) !== '/') {
+          return reply.redirect(`${fullUrlPathname}/`)
+        }
+
+        return reply.type('text/html').send(await render(`${currentDir}/view.hbs`, {
+          currentLanguage: locals.get('currentLanguage'),
+          defaultLanguage: locals.get('defaultLanguage')
+        }, {
+          title: 'Datasets'
+        }))
+      }
+      return handler
+    }
   }
 }
 
