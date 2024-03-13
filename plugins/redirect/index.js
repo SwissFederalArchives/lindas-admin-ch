ae2f224ab1c68e055dee35553278bfec90168d1e
Ludovic Muller <ludovicmuller1@gmail.com>
2024-03-13 06:49:32 +0100

    create redirect plugin for trailing slashes

---
+++ plugins/redirect/index.js
@@ -0,0 +1,27 @@
+/**
+ * This plugin redirects requests to the same URL with a trailing slash.
+ * This is useful to avoid duplicate content.
+ */
+
+const factory = async (_trifid) => {
+  return {
+    defaultConfiguration: async () => {
+      return {
+        methods: ['GET']
+      }
+    },
+    routeHandler: async () => {
+      const handler = async (request, reply) => {
+        const fullUrl = `${request.protocol}://${request.hostname}${request.raw.url}`
+        const fullUrlObject = new URL(fullUrl)
+        const fullUrlPathname = fullUrlObject.pathname
+
+        // Enforce trailing slash
+        return reply.code(301).redirect(`${fullUrlPathname}/`)
+      }
+      return handler
+    }
+  }
+}
+
+export default factory
