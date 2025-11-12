6d4537b5a96697f4fa2ac8ee3850c975f567cbfe
Giulio V <vannini.gv@gmail.com>
2025-11-12 08:37:57 +0100

    Fix no-control-regex linting error in xquery plugin
    Use inline eslint-disable comment to suppress false positive for
    intentional control character removal in security-sensitive regex

--- plugins/xquery/index.js
+++ plugins/xquery/index.js
@@ -103,8 +103,7 @@ const cleanupHeaderValue = (headerValue, defaultValue) => {
   }
 
   // Remove all control characters to prevent header injection
-  // eslint-disable-next-line no-control-regex
-  return newValue.replace(/[\x00-\x1F\x7F]/g, '')
+  return newValue.replace(/[\x00-\x1F\x7F]/g, '') // eslint-disable-line no-control-regex
 }
 
 export default factory
